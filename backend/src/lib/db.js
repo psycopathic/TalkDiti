761a32375c627b9e89c150f06431c30014d22e7f
psycopathic <harshsrivastava2502@gmail.com>
2026-02-07 12:09:36 +0530

    i have rephrased it

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,13 @@ import mongoose from 'mongoose'
 
 export const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 30000,
+            socketTimeoutMS: 45000,
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(`Encounted an error: ${error}`);
+        process.exit(1);
     }
 }
